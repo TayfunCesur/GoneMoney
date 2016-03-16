b0bf3b456e13211b43e694293001a44993daeb54
Tayfun Cesur <tayfuncesur35@gmail.com>
2016-03-17 00:48:53 +0200

    Update Report.java

--- app/src/main/java/xionces/com/gonemoney/Report.java
+++ app/src/main/java/xionces/com/gonemoney/Report.java
@@ -3,6 +3,7 @@ package xionces.com.gonemoney;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -17,6 +18,7 @@ public class Report extends AppCompatActivity{
 
     ListView list;
     List<Expense> recordlist;
+    List<String> newList;
     public static String[] months = new String[]{"January","February","March","April","May","June","July","August","September","October","November","December"};
     private List<String> different_months = new ArrayList<>();
     @Override
@@ -28,18 +30,34 @@ public class Report extends AppCompatActivity{
 
         list = (ListView) findViewById(R.id.listView2);
         recordlist = MainActivity.records;
+        newList = new ArrayList<>();
         int z = 0;
-        for (int i = 0; i<recordlist.size()-1;i++)
-        {
 
-            if (!recordlist.get(i).date.split("/")[1].equals(recordlist.get(i + 1).date.split("/")[1]))
+
+        for (int i = 0 ; i<recordlist.size();i++)
+        {
+            newList.add(recordlist.get(i).date.split("/")[1]);
+        }
+
+
+
+
+        if (newList.size() > 1 ) {
+
+            for (int i = 0;i<newList.size();i++)
             {
-                if (!different_months.contains(months[Integer.parseInt(recordlist.get(i).date.split("/")[1])-1]))
+                int k = 0;
+                for (int j = i+1 ;j<newList.size() -1; j++)
                 {
-                    different_months.add(months[Integer.parseInt(recordlist.get(i).date.split("/")[1])-1]);
-                    different_months.add(months[Integer.parseInt(recordlist.get(i+1).date.split("/")[1])-1]);
+                    if (newList.get(i).equals(newList.get(j)))
+                    {
+                        k++;
+                    }
                 }
+                if (k == 0) different_months.add(months[Integer.parseInt(newList.get(i)) - 1]);
             }
+        } else if (newList.size() != 0){
+            different_months.add(months[Integer.parseInt(newList.get(0)) - 1]);
         }
 
         ReportListAdapter adapter = new ReportListAdapter(getApplicationContext(),different_months);
@@ -50,7 +68,7 @@ public class Report extends AppCompatActivity{
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(Report.this,ReportDetails.class);
                 Bundle bundle = new Bundle();
-                bundle.putInt("pos",position);
+                bundle.putString("pos",months[Integer.parseInt(recordlist.get(position).date.split("/")[1])-1]);
                 intent.putExtras(bundle);
                 startActivity(intent);
             }
